575ce1a8cd5c34d4ce83705395de363f5e2dc4df
Sandrine <latour.sandrine.he@gmail.com>
2021-03-08 10:05:14 +0100

    Correction classe Utilisateur

--- menu.php
+++ menu.php
@@ -1,5 +1,13 @@
+<?php
 
 
-<link rel="stylesheet" href="Css/bootstrap-3.4.1-dist/css/bootstrap.min.css">
-<link rel ="stylesheet" href="Css/style.css">
-<link rel="stylesheet" href="Css/bootstrap-3.4.1-dist/css/bootstrap.css">
+require_once "../Database/Configuration.php";
+require_once "../Database/Database.php";
+require_once "../Models/UtilisateurClass.php";
+require_once "../Models/ClasseClass.php";
+require_once "../Models/UtilisateurManager.php";
+require_once "../Models/ClasseManager.php";
+?>
+<link rel="stylesheet" href="../Css/bootstrap-3.4.1-dist/css/bootstrap.min.css">
+<link rel ="stylesheet" href="../Css/style.css">
+<link rel="stylesheet" href="../Css/bootstrap-3.4.1-dist/css/bootstrap.css">
