3d4649aa4d9d29839a828bbfd4a15c85f6a9995a
Sandrine <latour.sandrine.he@gmail.com>
2021-03-10 11:47:33 +0100

    Css : add d'un top negatif pour accueil

--- Template/footer.php
+++ Template/footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
 Plome, solution de gestion des devoirs depuis février 2021
 <br />
-© 3iL Ingénieur | Renut Timimi Latour | 2 ème année ingénieur
+© 3iL Ingénieur | Renut Timimi Latour | 2ème année ingénieur
 </div>
 </body>
 </html>
