af8a563350acf02e8a7a366e2de8b2bdf6fd7421
Sandrine <latour.sandrine.he@gmail.com>
2021-03-10 11:31:35 +0100

    Ajout de la page connexion et accueil
    ajout de css

---
+++ Template/connexion.php
@@ -0,0 +1,45 @@
+<h1>Pour vous connecter</h1>
+
+<?php
+if(!isset($_POST['mail'])){
+	$mail = "";
+	$mdp = "";
+}
+else{
+	$mail = $_POST['mail'];
+	$mdp = $_POST['mdp'];
+}
+?>
+
+<form action="#" method="POST">
+	<span class="labelC">Mail 3iL : </span><br><input class="champ" type="text" name="login" value="<?php echo $mail; ?>"><br>
+	<span class="labelC">Mot de passe : </span><br><input class="champ" type="password" name="mdp" value="<?php echo $mdp; ?>"><br>
+	<input class="bouton" type="submit" value="Valider">
+</form>
+
+<?php
+if(isset($_POST['login'])){
+  Configuration::setConfigurationFile('Database/configuration.ini');
+  $db = Database::getInstance();
+	$manageruser = new UtilisateurManager($db);
+	$salt = "48@!alsd";
+	$mdp_crypte = sha1(sha1($mdp) . $salt);
+
+	$user = $manageruser->getUtAvecMailMdp($mail, $mdp_crypte);
+
+	if(!$user->isOk()){	// mauvais couple login/mdp
+	?>
+
+		<p>Erreur, login ou mot de passe incorrect.</p>
+
+	<?php }
+	else{ // connexion réussi
+
+		$_SESSION['idClasse'] = $user->getUtId();
+		$_SESSION['mail'] = $user->getUtMail();
+		header('Location: index.php');
+		exit();
+	}
+}
+
+?>
---
+++ Template/deconnexion.php
@@ -0,0 +1,7 @@
+<?php
+
+	$_SESSION['idClasse'] = null;
+	$_SESSION['mail'] = null;
+	header('Location: index.php');
+	exit();
+?>
