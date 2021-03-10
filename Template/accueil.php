4f0477522ba6cf397a7637a3c0d82674a959e0bf
Sandrine <latour.sandrine.he@gmail.com>
2021-03-10 11:39:45 +0100

    MàJ header
    orga des pages et ordre revue

--- Template/accueil.php
+++ Template/accueil.php
@@ -1,1 +1,42 @@
+
+<!-- CAROUSEL -->
+  <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <!-- Slide One - Set the background image for this slide in the line below -->
+      <div class="carousel-item active" style="background-image: url('Css/images/mainDiplome.jpg');">
+        <div class="carousel-caption d-none d-md-block">
+          <h3 class="display-4">Votre Diplome</h3>
+          <p class="lead">C'est cool d'obtenir son diplome !</p>
+        </div>
+      </div>
+      <!-- Slide Two - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('Css/images/biblio.jpg');">
+        <div class="carousel-caption d-none d-md-block">
+          <h3 class="display-4">Liste des devoirs</h3>
+          <p class="lead">La liste des devoirs vous permet de tenir à jour votre avancement !</p>
+        </div>
+      </div>
+      <!-- Slide Three - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('Css/images/travail.jpg');">
+        <div class="carousel-caption d-none d-md-block">
+          <h3 class="display-4">Organisez vos révisions</h3>
+          <p class="lead">Le semainier vous permet d'obtenir un point de vue de vos semaines à venir !</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Précédent</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Suivant</span>
+    </a>
+  </div>
+<!-- CAROUSEL -->
 <div class="containerAccueil"><img class="centreImage" src="Css/images/optention.jpg" alt="PLOME" title="PLOME" width="38%"/></div>
